8fca448f07c5d0fe737fdb8c6da54171363983bd
Russo Salvatore <russo.salvatore@osdbsesto>
2022-03-10 13:41:51 +0100

    finish 1 trial

--- es_cookie.php
+++ es_cookie.php
@@ -1,16 +1,29 @@
 <?php
-echo "<form action='es_cookie' method='post'>
-valore<input type='text' name='valore'></p>
+echo "<form action='es_cookie.php' method='post'>
+valore<input type='number' name='valore'></p>
+<input type='submit' name='invio'>
 </form>";
 if(!empty($_POST)){
-    if(!empty($_COOKIE)){
+    if(empty($_COOKIE['totale'])){
     setcookie("totale",$_POST["valore"]);  
-    setcookie("media",$_POST["1"]);
+    $med=1;
+    setcookie("media",$med);
     setcookie("massimo",$_POST["valore"]);
-    setcookie("minomo",$_POST["valore"]);
+    setcookie("minimo",$_POST["valore"]);
     }else{
         $tot=$_COOKIE['totale']+$_POST['valore'];
-        echo $tot;
-    }
-}
+        setcookie("totale",$tot); 
+        if($_COOKIE['massimo']<$_POST['valore']){
+            setcookie("massimo",$_POST["valore"]);   
+        }
+        if($_COOKIE['minimo']>$_POST['valore'])
+        setcookie("minimo",$_POST["valore"]);
+        }
+        $mid=$tot/($_COOKIE["media"]+1);
+        $mod=$_COOKIE["media"]+1;
+        setcookie("media",$mod);
+        echo $tot+"</br>";
+        echo $_COOKIE["massimo"]+"</br>";
+        echo $_COOKIE["minimo"]+"</br>";
+        echo $mid+"</br>";
 ?>
